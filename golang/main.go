75040977880304bb13e4aee65e283d2e6953816b
beleo <leo.benazeth@ynov.com>
2023-11-10 11:35:53 +0100

    finalité du localhost, recherche pour en savoir +

--- golang/main.go
+++ golang/main.go
@@ -4,22 +4,20 @@ import (
 	"fmt"
 	"time"
 	"net/http"
-	"text/template"
 )
 
-var templates = template.Must(template.ParseFiles("web/index.html"))
+
 var hidden_word []string // Variable sous forme de liste qui contient le mot caché
 var used_letters []string // Variable sous forme de liste qui contient les lettres utilisées
 
-type Page struct {
-	Valeur string
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Bienvenue sur la page d'accueil!</h1>")
 }
 
 func main() {
 
-	http.HandleFunc("/home", homeHandler)
-	http.ListenAndServe(":8080", nil) // Accet pour le port 8080
-	http.Handle("/", http.FileServer(http.Dir("static/"))) // A utiliser si notre site est static
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 
 	PrintHangmanAscii() // Appel de la fonction pour afficher "Hangman" en ascii
 	fmt.Println("Bienvenue sur Hangman !")
@@ -30,12 +28,4 @@ func main() {
 	originalWord := WordPicker(RandomNumber()) // Initalisation du mot aléatoire a faire deviner
 	Hidden(originalWord) // Modificiton du mot généré en underscore
 	RunHangman(originalWord, tries) // Lancement du jeu
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	p := Page{Valeur: "hagman web"}
-	err := templates.ExecuteTemplate(w, "web/index.html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
 }
