7cbf92259a597057f0632bccb77a67dd4001ef4b
beleo <leo.benazeth@ynov.com>
2023-11-17 09:21:32 +0100

    connextion entre le fichier main.go et index.html réussit

--- golang/main.go
+++ golang/main.go
@@ -4,28 +4,16 @@ import (
 	"fmt"
 	"time"
 	"net/http"
-	"text/template"
 )
 
-const port = ":8080"
 var hidden_word []string // Variable sous forme de liste qui contient le mot caché
 var used_letters []string // Variable sous forme de liste qui contient les lettres utilisées
 
-func home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
-	if err != nil {
-		fmt.Println("error")
-	}
-	t.Execute(w, nil)
-}
-
 func main() {
 
-	http.HandleFunc("/", home)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	http.ListenAndServe(":8080", nil)
 
 	PrintHangmanAscii() // Appel de la fonction pour afficher "Hangman" en ascii
 	fmt.Println("Bienvenue sur Hangman !")
@@ -36,7 +24,4 @@ func main() {
 	originalWord := WordPicker(RandomNumber()) // Initalisation du mot aléatoire a faire deviner
 	Hidden(originalWord) // Modificiton du mot généré en underscore
 	RunHangman(originalWord, tries) // Lancement du jeu
-
-	fmt.Println("(https://localhost:8080) - Serveur started on port", port)
-	http.ListenAndServe(port, nil)
 }
